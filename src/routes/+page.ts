94ff6354090e4c16b11963d43c5954c79130b1a8
Nelson Correia <nelson.ncdesigns@gmail.com>
2024-12-01 05:14:22 +0000

    refactor(page): setting up mbta object to support routes each block data

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,4 @@
-import { ApiConfig } from "$lib/js/classes.ts"
-import { TYPES } from "$lib/js/constants.ts"
+import { ApiConfig, TYPES } from "$lib"
 import "$lib/css/styles.styl"
 export const prerender = true
 
@@ -8,20 +7,33 @@ const types = TYPES;
 export function _buildData(){
     // Request and push routes of each rail to mbta in store
     const mbta = {
-        rails: {} as { [key: string]: { name: string, data: object } }
+        rails: {} as { [key: string]: { name: string, routes: object } }
     };
     
-    types.forEach(type=>{
+    types.forEach((type: object)=>{
       const rail = new ApiConfig(type.id,'routes?filter[type]=');
       
       rail.requestId = type.id;
       rail.requestString = type.id;
       
       rail.apiRequest()
-      .then(data=>{
-          return mbta.rails[type.id] = {name:type.name,data:data};
+      .then((data: object)=>{
+        let routeObjs = [];
+        for(let item in data){
+          let routeObj = {
+            index: item,
+            id: data[item].id
+          }
+          routeObjs.push(routeObj);
+        }
+        return mbta.rails[type.id] = {
+          name:type.name,
+          routes:routeObjs
+        };
       });
     });
     
     return mbta;
 }
+
+//https://www.typescriptlang.org/tsconfig/
