7db13052cedb7ea63f0da105111f94050875bc21
Nelson Correia <nelson.ncdesigns@gmail.com>
2024-12-29 00:13:56 +0000

    refactor(d3): implement svg instead of ul method

--- src/lib/components/Graph.svelte
+++ src/lib/components/Graph.svelte
@@ -1,80 +1,99 @@
 <script lang="ts">
-    import Select from '$lib/components/Select.svelte';
-    import * as d3 from 'd3';
-    export let graphData;
-    
-    function optionsBuilder(data: {index: string, id: string}[]){
-        let options=[];
-        for(let item in data){
-            options.push({
-                index: item,
-                id: graphData.rails[item].name
-            });
-        }
-        return options;
+  import Select from '$lib/components/Select.svelte';
+  import * as d3 from 'd3';
+  export let graphData;
+
+  let graphContainer;
+
+  function optionsBuilder(data: { index: string, id: string } []) {
+    let options = [];
+    for (const item in data) {
+      options.push({
+        index: item,
+        id: graphData.rails[item].name
+      });
     }
-    
-    let selectedChildValueRails = '';
-    let selectedChildValueRoutes = '';
-    
-    let optionRail;
-    let optionRoute;
-    $: optionRail = selectedChildValueRails;
-    $: optionRoute = selectedChildValueRoutes;
-    // If the rail selection has been changed, reset the route select
-    $: if (optionRail){
-        selectedChildValueRoutes = '';
-    }
-    // If the route selection has been changed, run the route data request
-    $: if (optionRail){
-        //const id=graphData.rails[optionRail].routes[optionRoute].id;
-        //let response = fetch(`https://api-v3.mbta.com/trips?filter[route]=${id}&api_key=1b8be318d66b41ba87c7e47dd32db7a4`);
-        
-        //console.log(graphData.rails[optionRail].routes[optionRoute]);
-        const data = graphData.rails[optionRail].routes;
-        const width = 30;
-        const height = 30;
-        const bg = 'red';
-        //const test = d3.select('.test');
-        const svg = d3.create("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("fill",bg);
-        
-        const container=d3.select('.graph');
-        
-        const containerWidth = container.node().getBoundingClientRect().width;
-        
-        container.html("")
-        .selectAll('li')
-        .data(data)
-        .enter()
-        .append('li')
-        .append('span')
-        .attr('class','bars')
-        .html(d => `<span>${d.id} = ${d.trips} trips</span>`)
-        .style('display','block')
-        .style('background-color','lightblue')
-        .style('margin','5px')
-        .style('padding','10px')
-        .style('width',0)
-        .transition()
-        .duration(500)
-        .ease(d3.easeLinear)
-        .style('width', d => {
-            return `${(d.trips/containerWidth) * 10}%`;
-        });
-        
-    }
-    
+    return options;
+  }
+
+  let selectedChildValueRails = '';
+  let selectedChildValueRoutes = '';
+
+  let optionRail;
+  let optionRoute;
+
+  $: optionRail = selectedChildValueRails;
+  $: optionRoute = selectedChildValueRoutes;
+  // If the rail selection has been changed, reset the route select
+  $: if (optionRail) {
+    selectedChildValueRoutes = '';
+  }
+  // If the route selection has been changed, run the route data request
+  $: if (optionRail) {
+
+    const data = graphData.rails[optionRail].routes;
+
+    const container = d3.select('#graphContainer');
+
+    const containerWidth = container.node().getBoundingClientRect().width;
+
+    d3
+      .select("#graphContainer")
+      .select("svg")
+      .remove();
+
+    const svg2 = d3
+      .select("#graphContainer")
+      .append("svg")
+      .attr('width', '100%');
+
+    const g = svg2
+      .selectAll("g")
+      .data(data)
+      .enter()
+      .append("g");
+
+    g.append("rect")
+      .attr("width", 0)
+      .attr("height", 20)
+      .attr("x", 0)
+      .attr("y", function(d, i) {
+        d.rectheight = i * 40;
+        return i * 40;
+
+      })
+      .attr("fill", "red")
+      .transition()
+      .duration(500)
+      .ease(d3.easeLinear)
+      .style('width', d => {
+        return `${(d.trips/containerWidth) * 10}%`;
+      });
+
+    svg2
+      .attr('height', function() {
+        const rects = d3.selectAll('svg g');
+        return rects._groups[0].length * 40;
+      });
+
+    g.append('text')
+      .text(function(d) {
+        return `${d.id} = ${d.trips} trips`;
+      })
+      .attr('x', '0')
+      .attr('y', function(d) {
+        console.log(d.rectheight + 15);
+        return d.rectheight + 15;
+      });
+  }
 </script>
 <form>
-    <Select name="rail" id="rail" bind:selected={selectedChildValueRails} data={optionsBuilder(graphData.rails)} />
-    {#if optionRail}
+  <Select name="rail" id="rail" bind:selected={selectedChildValueRails} data={optionsBuilder(graphData.rails)} />
+  {#if optionRail}
     <Select name="route" id="route" bind:selected={selectedChildValueRoutes} data={graphData.rails[optionRail].routes} />
     {/if}
 </form>
-<ul class="graph"></ul>
+<div id="graphContainer"><ul class="graph"></ul></div>
 <style lang="stylus">
 @import '../css/vars-functions.styl'
     
@@ -84,4 +103,7 @@ form
     box-shadow 0 0 3px 0 rgba(0,0,0,0.25)
     padding 1rem
     background-color var(--bg-color)
+g > text 
+    dominant-baseline text-before-edge
+    text-anchor middle
 </style>
