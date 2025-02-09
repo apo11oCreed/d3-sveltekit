f3b6c4e07804e7955fe4902060ff55e546fe9179
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-02-09 22:49:24 +0000

    feat(graph): clean up and x/y axis configs

--- src/lib/components/Graph.svelte
+++ src/lib/components/Graph.svelte
@@ -36,33 +36,26 @@
   }
   // If the route selection has been changed, run the route data request
   $: if (optionRail) {
-
-    // get routes data of selected rail
-    const data = graphData.rails[optionRail].routes;
-    // this does not work
-    // const tripData = data.forEach(d => {
-    //   return d.trips = +d.trips;
-    // });
-    
-    // get the target container
-    graphContainer = d3.select('#graphContainer');
-    graphContainerWidth = graphContainer.node().offsetWidth;
-    graphContainerStyles = window.getComputedStyle(graphContainer.node());
-    
-    console.log(graphContainerStyles.getPropertyValue('padding-left'));
-    
-    const margin = {top: 70, right: 40, bottom: 60, left: 175},
-    
-    width = (graphContainerWidth - (parseInt(graphContainerStyles.getPropertyValue('padding-left')) * 2)) - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-    
-    graphContainer.attr('style','border: 2px solid red');
     
     // remove existing svg
     d3
       .select('#graphContainer')
       .select('svg')
       .remove();
+
+    // get routes data of selected rail
+    const data = graphData.rails[optionRail].routes;
+    
+    // get the target container
+    graphContainer = d3.select('#graphContainer');
+    graphContainerWidth = graphContainer.node().offsetWidth;
+    graphContainerStyles = window.getComputedStyle(graphContainer.node());
+    
+    const margin = {top: 70, right: 40, bottom: 60, left: 175};
+    const width = (graphContainerWidth - (parseInt(graphContainerStyles.getPropertyValue('padding-left')) * 2)) - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+    
+    graphContainer.attr('style','border: 2px solid red');
     
     // add new svg
     const svg = d3
@@ -72,111 +65,76 @@
       .attr('height', height + margin.top + margin.bottom)
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-      
-    const g = svg
-      .selectAll("g")
-      .data(data)
-      .enter()
-      .append("g");
-      
-    const tripsArray=data.map(item=>{
-      return item.trips;
+    
+    data.sort(function (a,b) {
+      return d3.descending(a.id, b.trips);
     });
-      
+    
     const x = d3.scaleLinear()
-      .domain([0, d3.max(tripsArray)])
       .range([0, width])
+      .domain([0, d3.max(data,function(d){
+        return d.trips;
+      })])
       .nice();
-      
-    console.log(d3.max(tripsArray));
       
     const y = d3.scaleBand()
       .range([height, 0])
       .padding(0.1)
-      .domain(data.map(function (d) { return d.id; }));
+      .domain(data.map(function (d) {
+        return d.id;
+      }));
       
     const xAxis = d3.axisBottom(x);
     const yAxis = d3.axisLeft(y);
-    
+
     svg
       .append("g")
       .attr('class','x axis')
       .attr('transform','translate(0, ' + height + ')')
       // .attr("x1", "100%")
       .call(xAxis);
+      
+    svg
+      .append("g")
+      .call(yAxis);
+      
+    svg.selectAll('.bar')
+    .data(data)
+    .enter().append('rect')
+    .attr('class','bar')
+    .attr('y', function(d) {
+      return y(d.id);
+    })
+    .attr('height', y.bandwidth())
+    .attr('x', 0)
+    .attr('width', function(d) {
+      return x(d.trips);
+    })
+    .attr("fill", d => {
+      let color;
+      switch(optionRail){
+        case '0':
+          color = getSubwayColor(d.id);
+        break;
+        case '1':
+          color = getSubwayColor(d.id);
+        break;
+        case '2':
+          color = '#80276c';
+        break;
+        case '3':
+          color = '#ffc72c';
+        break;
+        case '4':
+          color = '#008eaa';
+        break;
+        default:
+          color = '#494f5c';
+        break;
+      }
+      return color;
+    });
     
-    
-      
-    d3
-      .ticks(1, 9, 5);
-      
-    // const xScale = d3.scaleLinear()
-    //   .domain([0, 100])
-    //   .range([0, 200]);
-      
-    // const xAxis = d3.axisBottom(xScale)
-    //   .ticks(10);
-
-    g.append("rect")
-      .attr("width", 0)
-      .attr("height", 20)
-      .attr("x", 0)
-      .attr("y", function(d, i) {
-        d.rectheight = i * 40;
-        return i * 40;
-
-      })
-      .attr("fill", d => {
-        let color;
-        switch(optionRail){
-          case '0':
-            color = getSubwayColor(d.id);
-          break;
-          case '1':
-            color = getSubwayColor(d.id);
-          break;
-          case '2':
-            color = '#80276c';
-          break;
-          case '3':
-            color = '#ffc72c';
-          break;
-          case '4':
-            color = '#008eaa';
-          break;
-          default:
-            color = '#494f5c';
-          break;
-        }
-        return color;
-      })
-      .transition()
-      .duration(500)
-      .ease(d3.easeLinear)
-      .style('width', d => {
-        return `${(d.trips/width) * 10}%`;
-      });
-      
-    // svg
-    //   .attr('height', function() {
-    //     const rects = d3.selectAll('svg g');
-    //     return rects._groups[0].length * 40;
-    //   })
-    //   .attr('width',graphContainerWidth)
-    //   .append("g")
-    //   .attr('class','x axis')
-    //   .attr('transform','translate(0,' + height + ')')
-    //   .attr("x1", "100%")
-    //   .call(xAxis);
-
-    g.append('text')
-      .text(function(d) {
-        return `${d.id} = ${d.trips} trips`;
-      })
-      .attr('x', '0')
-      .attr('y', function(d) {
-        return d.rectheight + 15;
-      });
   }
   
   function getSubwayColor(id){
