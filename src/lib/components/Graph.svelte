4b2b874d8e32029f9f3df635c16bc4a3790e35e0
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-03-15 19:30:28 +0000

    feat(runes): import destructuring for props

--- src/lib/components/Graph.svelte
+++ src/lib/components/Graph.svelte
@@ -2,8 +2,7 @@
   import Select from '$lib/components/Select.svelte';
   import { SUBWAYCOLORS } from '$lib/js/constants';
   import * as d3 from 'd3';
-  export let dataInput;
-  export let optionRailInput;
+  let {dataInput,optionRailInput} = $props();
 
   type RouteData = { id: string; trips: number };
   
@@ -13,117 +12,124 @@
   
   
   // If the route selection has been changed, run the route data request
-  $: if (optionRailInput) {
-    
-    // remove existing svg
-    d3
-      .select('#graphContainer')
-      .select('svg')
-      .remove();
-
-    // get routes data of selected rail
-    const data = dataInput.rails[optionRailInput].routes;
-    
-    // get the target container
-    graphContainer = d3.select('#graphContainer').node() as HTMLElement;
-    graphContainerWidth = graphContainer.offsetWidth;
-    graphContainerStyles = window.getComputedStyle(graphContainer);
-    
-    const margin = {top: 16, right: 75, bottom: 32, left: 125};
-    const width = (graphContainerWidth - (parseInt(graphContainerStyles.getPropertyValue('padding-left')) * 2)) - margin.left - margin.right;
-    const barHeight = 30;
-    const barSpacing = 5;
-    const chartHeight = (data.length * (barHeight + barSpacing)) - margin.top - margin.bottom;
-    
-    // add new svg
-    const svg = d3
-      .select('#graphContainer')
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', chartHeight + margin.top + margin.bottom)
-      .append('g')
-      .attr('fill','#b3b3b3')
-      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-    
-    data.sort(function (a: { id: string, trips: number }, b: { id: string, trips: number }) {
-      return d3.descending(a.trips, b.trips);
-    });
-    
-    // setup x axis scale
-    const x = d3.scaleLinear()
-      .range([0, width])
-      .domain([0, d3.max(data,function(d: {id:string, trips:number}){
-        return d.trips;
-      }) || 0])
-      .nice();
+  $effect(()=>{ if (optionRailInput) {
       
-    // setup y axis scale
-    const y = d3.scaleBand()
-      .range([chartHeight, 0])
-      .padding(0.1)
-      .paddingInner(0.5)
-      .domain(data.map(function (d:{id:string, trips:number}) {
-        return d.id;
-      }));
-    
-    // setup orientation of ticks on the y and x axis
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y);
-
-    // create the groups, setup the position and assign props
-    svg
-      .append("g")
-      .attr('class','x axis')
-      .attr('transform','translate(0, ' + chartHeight + ')')
-      .call(xAxis);
+      // remove existing svg
+      d3
+        .select('#graphContainer')
+        .select('svg')
+        .remove();
+  
+      // get routes data of selected rail
+      const data = dataInput.rails[optionRailInput].routes;
       
-    svg
-      .append("g")
-      .attr('class','y axis')
-      .call(yAxis);
+      // get the target container
+      graphContainer = d3.select('#graphContainer').node() as HTMLElement;
+      graphContainerWidth = graphContainer.offsetWidth;
+      graphContainerStyles = window.getComputedStyle(graphContainer);
+      
+      const margin = {top: 16, right: 75, bottom: 32, left: 125};
+      const width = (graphContainerWidth - (parseInt(graphContainerStyles.getPropertyValue('padding-left')) * 2)) - margin.left - margin.right;
+      const barHeight = 30;
+      const barSpacing = 5;
+      const chartHeight = (data.length * (barHeight + barSpacing)) - margin.top - margin.bottom;
+      
+      // add new svg
+      const svg = d3
+        .select('#graphContainer')
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', chartHeight + margin.top + margin.bottom)
+        .append('g')
+        .attr('fill','#b3b3b3')
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+      
+      data.sort(function (a: { id: string, trips: number }, b: { id: string, trips: number }) {
+        return d3.descending(a.trips, b.trips);
+      });
+      
+      // setup x axis scale
+      const x = d3.scaleLinear()
+        .range([0, width])
+        .domain([0, d3.max(data,function(d: {id:string, trips:number}){
+          console.log(d);
+          return d.trips;
+        }) || 0])
+        .nice();
+        
+      // setup y axis scale
+      const y = d3.scaleBand()
+        .range([chartHeight, 0])
+        .padding(0.1)
+        .paddingInner(0.5)
+        .domain(data.map(function (d:{id:string, trips:number}) {
+          return d.id;
+        }));
+      
+      // setup orientation of ticks on the y and x axis
+      const xAxis = d3.axisBottom(x);
+      const yAxis = d3.axisLeft(y);
+  
+      // create the groups, setup the position and assign props
+      svg
+        .append("g")
+        .attr('class','x axis')
+        .attr('transform','translate(0, ' + chartHeight + ')')
+        .call(xAxis);
+        
+      svg
+        .append("g")
+        .attr('class','y axis')
+        .call(yAxis);
+      
+      // create the bars and assign bar props
+      svg.selectAll('.bar')
+      .data(data as RouteData[])
+      .enter()
+      .append('rect')
+      .attr('class','bar')
+      .attr('y', (d: RouteData) =>{
+        const yValue = y(d.id);
+        // This ensures that the y attribute is always set to a valid number, preventing potential issues if the y scale function returns undefined
+        return yValue !== undefined ? yValue : 0;
+      })
+      .attr('height', y.bandwidth())
+      .attr('x', 0)
+      .attr('width', (d: RouteData) =>{
+        return x(d.trips);
+      })
+      .attr("fill", d => {
+        let color;
+        switch(optionRailInput){
+          case '0':
+            color = getSubwayColor(d.id);
+          break;
+          case '1':
+            color = getSubwayColor(d.id);
+          break;
+          case '2':
+            color = '#80276c';
+          break;
+          case '3':
+            color = '#ffc72c';
+          break;
+          case '4':
+            color = '#008eaa';
+          break;
+          default:
+            color = '#494f5c';
+          break;
+        }
+        return color;
+      });
+      
+      svg.select('.y.axis')
+        .selectAll('text')
+        .attr('style','font-size: 0.8rem;font-weight:700;');
+      
+    }
     
-    // create the bars and assign bar props
-    svg.selectAll('.bar')
-    .data(data as RouteData[])
-    .enter()
-    .append('rect')
-    .attr('class','bar')
-    .attr('y', (d: RouteData) =>{
-      const yValue = y(d.id);
-      // This ensures that the y attribute is always set to a valid number, preventing potential issues if the y scale function returns undefined
-      return yValue !== undefined ? yValue : 0;
-    })
-    .attr('height', y.bandwidth())
-    .attr('x', 0)
-    .attr('width', (d: RouteData) =>{
-      return x(d.trips);
-    })
-    .attr("fill", d => {
-      let color;
-      switch(optionRailInput){
-        case '0':
-          color = getSubwayColor(d.id);
-        break;
-        case '1':
-          color = getSubwayColor(d.id);
-        break;
-        case '2':
-          color = '#80276c';
-        break;
-        case '3':
-          color = '#ffc72c';
-        break;
-        case '4':
-          color = '#008eaa';
-        break;
-        default:
-          color = '#494f5c';
-        break;
-      }
-      return color;
-    });
-    
-  }
+  });
   
   function getSubwayColor(id: string){
     for(const route in SUBWAYCOLORS){
