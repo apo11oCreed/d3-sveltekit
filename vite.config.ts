9dcd229956d1788189c87d7a3d5ef0100c1e2808
Nelson Correia <nelson.ncdesigns@gmail.com>
2025-03-30 10:01:10 +0000

    chore(configs): add aws to allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+	  allowedHosts: [
+	    'a8d9241a6c0348e692ff1a3860ec0838.vfs.cloud9.us-east-1.amazonaws.com'
+	    ]
+	}
 });
